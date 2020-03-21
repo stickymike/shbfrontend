b6ba1c57f8c2e511463491bf8d249d556167eab4
stickymike <nickwdunn@gmail.com>
2020-03-21 14:16:56 -0400

    updated more packages

--- src/config/apollo.ts
+++ src/config/apollo.ts
@@ -1,11 +1,9 @@
-// import ApolloClient from "apollo-boost";
 import { endpoint, prodEndpoint } from "./config";
 import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 import { onError } from "apollo-link-error";
 import { ApolloLink } from "apollo-link";
-// import gql from "graphql-tag";
 
 const cache = new InMemoryCache({
   cacheRedirects: {
